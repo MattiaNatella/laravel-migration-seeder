8ce5df0f6ac0ba827a82fd8b4a409e7df6bde543
MattiaNatella <mattianatella@gmail.com>
2025-05-21 19:36:56 +0200

    feat: modified font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    @vite(['resources/sass/app.scss', "resources/js/app.js"])
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orario dei treni</title>
