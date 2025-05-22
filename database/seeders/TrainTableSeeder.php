63f7c0fd24ce06dcfbddae763893736fcec0f375
MattiaNatella <mattianatella@gmail.com>
2025-05-22 17:17:32 +0200

    feat: implement TrainTableSeeder and update welcome view for train display

---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        for ($i = 0; $i < 25; $i++) {
+
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->company() . ' ' . $faker->companySuffix();
+            $newTrain->stazione_partenza = $faker->city();
+            $newTrain->stazione_arrivo = $faker->city();
+            $newTrain->orario_partenza = $faker->time();
+            $newTrain->orario_arrivo = $faker->time();
+            $newTrain->data_di_partenza = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->posti_disponibili = $faker->numberBetween(0, 400);
+            $newTrain->codice_treno = $faker->bothify('??-##########');
+            $newTrain->totale_carrozze = $faker->numberBetween(7, 20);
+            $newTrain->tipo_treno = $faker->word();
+            $newTrain->stato_treno = $faker->boolean(0.5);
+            $newTrain->cancellato = $faker->boolean(0.5);
+            $newTrain->in_ritardo = $faker->boolean(0.5);
+
+            $newTrain->save();
+        }
+
+
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,15 +13,24 @@
     <div class="container">
         <h1>Orario dei treni disponibili attualmente</h1>
 
-        @foreach ($futureAvailableTrains as $train)
-            <div class="train">
-                <h2>Treno {{ $train['codice_treno'] }}</h2>
-                <p>Stazione di Partenza: {{ $train['stazione_partenza'] }}</p>
-                <p>Stazione di Arrivo: {{ $train['stazione_arrivo'] }}</p>
-                <p>Orario di Partenza: {{ $train['orario_partenza'] }}</p>
-                <p>Orario di Arrivo: {{ $train['orario_arrivo'] }}</p>
+        @if (empty($futureAvailableTrains))
+            <h2>Nessun treno disponibile</h2>
+        @else
+
+            <div class="train row row-cols-4">
+                @foreach ($futureAvailableTrains as $train)
+                    <div class="col border border-5 border-info rounded-2 p-1 m-1">
+
+                        <h2>Treno {{ $train['codice_treno'] }}</h2>
+                        <p>Stazione di Partenza: {{ $train['stazione_partenza'] }}</p>
+                        <p>Stazione di Arrivo: {{ $train['stazione_arrivo'] }}</p>
+                        <p>Orario di Partenza: {{ $train['orario_partenza'] }}</p>
+                        <p>Orario di Arrivo: {{ $train['orario_arrivo'] }}</p>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        @endif
+
 
     </div>
 </body>
