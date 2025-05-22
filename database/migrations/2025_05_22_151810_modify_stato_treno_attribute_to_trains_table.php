668934d0942d38e3dc44107a42af72b94c5e3179
MattiaNatella <mattianatella@gmail.com>
2025-05-22 17:26:29 +0200

    feat: modify stato_treno attribute type in trains table migration

---
+++ database/migrations/2025_05_22_151810_modify_stato_treno_attribute_to_trains_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->boolean('stato_treno')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('stato_treno')->change();
+        });
+    }
+};
